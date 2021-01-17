26ce03869bc35213c4143dbfec4c69209e7fe63e
Ben <benpham_12@yahoo.com>
2021-01-17 14:12:12 -0800

    Fixed part2 tests

--- turnin/bpham028_lab2_part2_tests.py
+++ turnin/bpham028_lab2_part2_tests.py
@@ -21,15 +21,15 @@
 
 tests = [ {'description': 'PINA: 0x00 => PORTC: 0x04',
     'steps': [ {'inputs': [('PINA',0x00)], 'iterations': 5 } ],
-    'expected': [('PORTB',0x04)],
+    'expected': [('PORTC',0x04)],
     },
     {'description': 'PINA: 0x01 => PORTC: 0x03',
     'steps': [ {'inputs': [('PINA',0x01)], 'iterations': 5 } ],
-    'expected': [('PORTB',0x03)],
+    'expected': [('PORTC',0x03)],
     },
     {'description': 'PINA: 0x03 => PORTC: 0x02',
     'steps': [ {'inputs': [('PINA',0x03)], 'iterations': 5 } ],
-    'expected': [('PORTB',0x02)],
+    'expected': [('PORTC',0x02)],
     },
     ]
 #watch = ['PORTB']
